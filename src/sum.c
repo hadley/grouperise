529b88375477e79cf0a245e80553450b90ba08be
Hadley Wickham <h.wickham@gmail.com>
2019-10-21 09:23:22 -0500

    Reorder C code a little

--- src/sum.c
+++ src/sum.c
@@ -8,15 +8,13 @@ SEXP grouped_sum_dbl(SEXP x, SEXP g, SEXP m_) {
   int m = Rf_asInteger(m_);
 
   SEXP out = PROTECT(Rf_allocVector(REALSXP, m));
-
-  double *p_x = REAL(x);
-  int *p_g = INTEGER(g);
   double *p_out = REAL(out);
-
   for (int j = 0; j < m; ++j) {
     p_out[j] = 0;
   }
 
+  double *p_x = REAL(x);
+  int *p_g = INTEGER(g);
   for (int i = 0; i < n; ++i) {
     int g = p_g[i] - 1;
     p_out[g] += p_x[i];
