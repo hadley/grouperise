affed3f8f9b3ee5d4845fed4f56310bdedd95ef1
Hadley Wickham <h.wickham@gmail.com>
2019-10-22 06:59:24 -0500

    More refactoring

--- src/sum.c
+++ src/sum.c
@@ -4,15 +4,12 @@
 
 // Assume x and g are the same length
 SEXP grouped_sum_dbl(SEXP x, SEXP g, SEXP m_) {
-  int n = Rf_length(x);
   int m = Rf_asInteger(m_);
-
   SEXP out = PROTECT(Rf_allocVector(REALSXP, m));
   double *p_out = REAL(out);
-  for (int j = 0; j < m; ++j) {
-    p_out[j] = 0;
-  }
+  memset(p_out, 0, m * sizeof(int));
 
+  int n = Rf_length(x);
   double *p_x = REAL(x);
   int *p_g = INTEGER(g);
   for (int i = 0; i < n; ++i) {
